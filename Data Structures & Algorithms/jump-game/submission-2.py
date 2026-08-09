6525047979df2b7515346917158955f904c95e90
Muhammad Arham <103679355+iamArham10@users.noreply.github.com>
2026-08-09 19:03:06 +0500

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = {}
+        def reach(nums, i):
+            
+            if i == len(nums)-1:
+                return True
+
+            if i in memo:
+                return memo[i]
+
+            result = False
+
+            for j in range(1, nums[i]+1):
+                if (i + j < len(nums)):
+                    memo[i+j] = reach(nums, i + j)
+                    result = result or memo[i+j]
+            
+            return result
+        
+        return reach(nums, 0)
+
+
+
+
+        
